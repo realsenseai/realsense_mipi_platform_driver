b7c7239a6b8c2bcdf7755029161ddb74504e57c2
Wojciech Gladysz <kontra.wojciech.gladysz@gmail.com>
2025-12-15 12:39:53 +0100

    fixed some errors reported by lsp

--- test/test_fw_version.py
+++ test/test_fw_version.py
@@ -9,10 +9,10 @@ def test_fw_version(device):
         assert result == 0
 
         std_output = subprocess.check_output(["v4l2-ctl", "-d"+device, "-C", "fw_version"])
-        assert "fw version: " in std_output, "Couldn't fetch FW version"
+        assert "fw version: " in std_output.decode(), "Couldn't fetch FW version"
 
         # Remove the 'fw version: ' string from std output
-        fw_version = int(std_output.replace("fw version: ", ""))
+        fw_version = int(std_output.decode().replace("fw version: ", ""))
 
         fw_version_str = str(fw_version>>24 & 0xFF) + "." + str(fw_version>>16 & 0xFF) + "." + str(fw_version>>8 & 0xFF)  + "." + str(fw_version & 0xFF)
         print ("fw_version:", fw_version_str)
@@ -22,7 +22,7 @@ def test_fw_version(device):
 
         # Get DFU device name
         dfu_device = subprocess.check_output(["ls", "/sys/class/d4xx-class/"])
-        assert "d4xx-dfu-" in dfu_device, "D4xx DFU device not found"
+        assert "d4xx-dfu-" in dfu_device.decode(), "D4xx DFU device not found"
 
         # Get FW version from DFU device info
         dfu_device_info = subprocess.check_output(["cat", "/dev/"+dfu_device.strip()])
