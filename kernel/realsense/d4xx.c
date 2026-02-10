e620e2ed8270ae1d53c25fddf5fd2f32a1caf98c
ymodlin <yogev.modlin@realsenseai.com>
2026-02-10 16:49:02 +0200

    Bump module version to 1.0.2.6

--- kernel/realsense/d4xx.c
+++ kernel/realsense/d4xx.c
@@ -6432,4 +6432,4 @@ MODULE_AUTHOR("Guennadi Liakhovetski <guennadi.liakhovetski@intel.com>,\n\
 				Shikun Ding <shikun.ding@intel.com>,\n\
 				Dmitry Perchanov <dmitry.perchanov@intel.com>");
 MODULE_LICENSE("GPL v2");
-MODULE_VERSION("1.0.2.5");
+MODULE_VERSION("1.0.2.6");
