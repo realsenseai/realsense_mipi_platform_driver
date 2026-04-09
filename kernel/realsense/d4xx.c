83ff319c7a0039e8db56c40029b7a4f535943e4d
Nikolai-L <nlyskov@realsenseai.com>
2026-04-09 18:45:29 +0300

    Bump driver version to 1.0.2.27

--- kernel/realsense/d4xx.c
+++ kernel/realsense/d4xx.c
@@ -6855,4 +6855,4 @@ MODULE_AUTHOR("Guennadi Liakhovetski <guennadi.liakhovetski@intel.com>,\n\
 				Shikun Ding <shikun.ding@intel.com>,\n\
 				Dmitry Perchanov <dmitry.perchanov@intel.com>");
 MODULE_LICENSE("GPL v2");
-MODULE_VERSION("1.0.2.26");
+MODULE_VERSION("1.0.2.27");
