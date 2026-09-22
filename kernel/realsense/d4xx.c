792413866ed9966560d481e13b39b63432acbb21
sysrsbuild-gh <sys_rsbuild@realsenseai.com>
2026-09-23 00:41:59 +0300

    Auto-increment version to 1.0.7.2

--- kernel/realsense/d4xx.c
+++ kernel/realsense/d4xx.c
@@ -9450,4 +9450,4 @@ MODULE_AUTHOR("Guennadi Liakhovetski <guennadi.liakhovetski@intel.com>,\n\
 				Shikun Ding <shikun.ding@intel.com>,\n\
 				Dmitry Perchanov <dmitry.perchanov@intel.com>");
 MODULE_LICENSE("GPL v2");
-MODULE_VERSION("1.0.7.1");
+MODULE_VERSION("1.0.7.2");
